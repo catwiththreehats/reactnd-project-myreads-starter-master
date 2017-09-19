740b8e6c42f4b6ca0582995a8bbbeba37f5628a0
Achim Kuehn <a.kuehn@e-llusions.de>
2017-09-19 14:13:27 +0200

    basic implementation of shelves and search

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI';
 import './App.css';
 
 class BooksApp extends React.Component {
@@ -10,19 +10,34 @@ class BooksApp extends React.Component {
     books: []
   }
 
+  componentDidMount() {
+    this.refreshBooks();
+  }
+
+  changeBookShelf = (book, targetShelf) => {
+    BooksAPI.update(book, targetShelf).then((result)=>{
+      this.refreshBooks();
+    });
+  }
+
+  refreshBooks = () => {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books });
+    });
+  }
+
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <ListBooks
-            books={this.state.books} />
+          <ListBooks books={this.state.books} onChangeBookShelf={this.changeBookShelf} />
         )}
         />
         <Route path="/search" render={({ history }) => (
-          <SearchBooks />
+          <SearchBooks onChangeBookShelf={this.changeBookShelf} books={this.state.books} />
         )} />
       </div>
-    )
+    );
   }
 }
 
